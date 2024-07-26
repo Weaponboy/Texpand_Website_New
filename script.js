adf7e2a4266b6d5d54e0fa1aba4ccf9d3ad3087e
Josh Anderson <111634244+Weaponboy@users.noreply.github.com>
2024-07-26 15:16:01 +0200

    Added the Mission row on home page

---
+++ script.js
@@ -0,0 +1,7 @@
+function openIgniteWebsite() {
+    window.open("https://www.igniteroboticsinternational.org/")
+}
+
+function openIntroVideo() {
+    window.open("https://www.youtube.com/watch?v=V2cJhmhxSt0")
+}
