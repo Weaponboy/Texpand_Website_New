2cc5c134e3a609bc752420f6348802a0fe389429
Josh Anderson <111634244+Weaponboy@users.noreply.github.com>
2024-08-06 19:42:12 +0200

    Finished home page first draft

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function openIgniteWebsite() {
 function openIntroVideo() {
     window.open("https://www.youtube.com/watch?v=V2cJhmhxSt0")
 }
+
+function openYoutube() {
+    window.open("https://www.youtube.com/@Texpand")
+}
+
+function openInstgram() {
+    window.open("https://www.instagram.com/team_texpand?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw%3D%3D")
+}
