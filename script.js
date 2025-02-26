7217ba8e68034bd1eba2fc9b5214d314c885ff35
Weaponboy <joshandy06@gmail.com>
2025-02-26 21:44:17 +0200

    New updated desktop working

--- script.js
+++ script.js
@@ -13,3 +13,66 @@ function openYoutube() {
 function openInstgram() {
     window.open("https://www.instagram.com/team_texpand?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw%3D%3D")
 }
+
+$("#Home").css({ "display": "block" });
+$("#AboutUs").css({ "display": "none" });
+$("#Outreach").css({ "display": "none" });
+$("#Sponsorships").css({ "display": "none" });
+$("#PastBots").css({ "display": "none" });
+
+$("#aboutUsTab").click(function () {
+
+    $("#Home").css({ "display": "none" });
+    $("#AboutUs").css({ "display": "block" });
+    $("#Outreach").css({ "display": "none" });
+    $("#Sponsorships").css({ "display": "none" });
+    $("#PastBots").css({ "display": "none" });
+
+});
+
+$("#homeTab").click(function () {
+
+    $("#Home").css({ "display": "block" });
+    $("#AboutUs").css({ "display": "none" });
+    $("#Outreach").css({ "display": "none" });
+    $("#Sponsorships").css({ "display": "none" });
+    $("#PastBots").css({ "display": "none" });
+
+});
+
+$("#outreachTab").click(function () {
+
+    $("#Home").css({ "display": "none" });
+    $("#AboutUs").css({ "display": "none" });
+    $("#Outreach").css({ "display": "block" });
+    $("#Sponsorships").css({ "display": "none" });
+    $("#PastBots").css({ "display": "none" });
+
+});
+
+$("#sponsorshipsTab").click(function () {
+
+    $("#Home").css({ "display": "none" });
+    $("#AboutUs").css({ "display": "none" });
+    $("#Outreach").css({ "display": "none" });
+    $("#Sponsorships").css({ "display": "block" });
+    $("#PastBots").css({ "display": "none" });
+
+});
+
+$("#pastRobotsTab").click(function () {
+
+    $("#Home").css({ "display": "none" });
+    $("#AboutUs").css({ "display": "none" });
+    $("#Outreach").css({ "display": "none" });
+    $("#Sponsorships").css({ "display": "none" });
+    $("#PastBots").css({ "display": "block" });
+
+});
+
+$("#button").click(function () {
+
+    $("#navButtons").toggleClass("test2");
+
+});
+
